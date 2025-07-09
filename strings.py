a6471352bc3627023f3ac7093be3981cab789638
arokya2648 <arokyasengooka@gmail.com>
2025-07-09 21:19:44 +0700

    string reversqal

---
+++ strings.py
@@ -0,0 +1,7 @@
+text=str(input("Enter a string:"))
+#reverse string
+#use step value as -1 to itirate in reverse
+revText=text[::-1]
+text =revText
+print("The reverse of this string is:")
+print(text)
