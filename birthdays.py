e5031e4a522b772621e2fb95182e06fc10710e50
arokya2648 <arokyasengooka@gmail.com>
2025-07-09 23:49:06 +0700

    Birthdays of 5 friends

---
+++ birthdays.py
@@ -0,0 +1,11 @@
+print("Birthdays of 5 friends:")
+a="1/7/2008"
+print("\nTee-", a)
+b="15/11/2007"
+print("\nArsh-", b)
+c="5/6/2008"
+print("\nKabeer-", c)
+d="26/12/2007"
+print("\nHarsath", d)
+e="7/11/2007"
+print("\nPrankur-", e)
