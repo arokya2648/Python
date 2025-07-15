3046d4ecf9f775b12196eccd2cdf85a56a54e3f7
arokya2648 <arokyasengooka@gmail.com>
2025-07-15 11:27:49 +0700

    if else

---
+++ checking number.py
@@ -0,0 +1,8 @@
+i=int(input("enter a number:"))
+if(i<15):
+    print("i is smaller than 15")
+    print("I'm in the if block")
+else:
+    print("i is greater than 15")
+    print("I'm in the else block")
+print("I'm not in if and not in else block")
