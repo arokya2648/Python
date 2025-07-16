42ba0022da5c26e51db106fc8468e92580732650
arokya2648 <arokyasengooka@gmail.com>
2025-07-16 21:11:56 +0700

    elif 2

---
+++ elif2.py
@@ -0,0 +1,17 @@
+a=10
+b=12
+c=12
+print(a!=b)
+print(b!=c)
+
+d="python"
+e="coding"
+if d !=e:
+    print(d, 'and', e, 'are different')
+f=4
+g=5
+if(f==1)!=(g==5):
+    print("Hello")
+h=int(input("enter a number"))
+if h%2!=0:
+    print(h, "is not a even number")
