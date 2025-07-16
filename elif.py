5127e4a23a83d1c44b02ac095024946e20c72662
arokya2648 <arokyasengooka@gmail.com>
2025-07-16 21:00:30 +0700

    elif 1

---
+++ elif.py
@@ -0,0 +1,17 @@
+a=10
+b=10
+c=0
+if a and b and c:
+    print("all of the numbers have boolean value as true")
+else:
+    print("Atleast one number has boolean value as false")
+
+
+if a>0 or b>0:
+    print("Either of the numbers is greater than 0")
+else:
+    print('No number is greater than 0')
+if b>0 and c>0:
+    print("Either of the nubers is greater than 0")
+else:
+    print("No number is greater than 0")
---
+++ temperature.py
@@ -0,0 +1,5 @@
+temp=int(input("Enter the temperature in Farenheit \n"))
+if temp>90 :
+    print('Wear cotton clothes')
+else:
+    print("Wear normal clothes")
