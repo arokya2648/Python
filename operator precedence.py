283bbf782bb09aa61f4c9347735b4ea778de7add
arokya2648 <arokyasengooka@gmail.com>
2025-07-22 11:16:18 +0700

    operator

---
+++ operator precedence.py
@@ -0,0 +1,14 @@
+v=4
+w=5
+x=6
+y=7
+z=0
+a= (v+w)*x/y
+print("Value of (v+w)*x/y is", a)
+
+name="Alex"
+age=0
+if name=="Alex" or name=="John" and age>=2:
+    print('Hello, welcome!')
+else:
+    print('good bye!!!')
