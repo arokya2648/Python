c6073d5526728f933d49e084dbbbca17ef11b287
arokya2648 <arokyasengooka@gmail.com>
2025-07-22 11:26:29 +0700

    diviibility

---
+++ divisible number.py
@@ -0,0 +1,7 @@
+numn=int(input("Enter a number (Numerator):"))
+numd=int(input("Enter a number (Denominator):"))
+
+if numn%numd==0:
+    print("\n Numerator is divisible by Denominator")
+else:
+    print('\n Numerator is not divisible by denominator')
