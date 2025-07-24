286996851d21197744fb530be5942a931974294a
arokya2648 <arokyasengooka@gmail.com>
2025-07-24 21:27:41 +0700

    ASCII

---
+++ ASCII.py
@@ -0,0 +1,56 @@
+x = input("Enter the ASCII Code between 65-90: ")
+
+if x == "65":
+    print("The ASCII Code 65 stands for Alphabet A")
+elif x == "66":
+    print("The ASCII Code 66 stands for Alphabet B")
+elif x == "67":
+    print("The ASCII Code 67 stands for the Alphabet C")
+elif x == "68":
+    print("The ASCII Code 68 stands for the Alphabet D")
+elif x == "69":
+    print("The ASCII Code 69 stands for the Alphabet E")
+elif x == "70":
+    print("The ASCII Code 70 stands for the Alphabet F")
+elif x == "71":
+    print("The ASCII Code 71 stands for the Alphabet G")
+elif x == "72":
+    print("The ASCII Code 72 stands for the Alphabet H")
+elif x == "73":
+    print("The ASCII Code 73 stands for the Alphabet I")
+elif x == "74":
+    print("The ASCII Code 74 stands for the Alphabet J")
+elif x == "75":
+    print("The ASCII Code 75 stands for the Alphabet K")
+elif x == "76":
+    print("The ASCII Code 76 stands for the Alphabet L")
+elif x == "77":
+    print("The ASCII Code 77 stands for the Alphabet M")
+elif x == "78":
+    print("The ASCII Code 78 stands for the Alphabet N")
+elif x == "79":
+    print("The ASCII Code 79 stands for the Alphabet O")
+elif x == "80":
+    print("The ASCII Code 80 stands for the Alphabet P")
+elif x == "81":
+    print("The ASCII Code 81 stands for the Alphabet Q")
+elif x == "82":
+    print("The ASCII Code 82 stands for the Alphabet R")
+elif x == "83":
+    print("The ASCII Code 83 stands for the Alphabet S")
+elif x == "84":
+    print("The ASCII Code 84 stands for the Alphabet T")
+elif x == "85":
+    print("The ASCII Code 85 stands for the Alphabet U")
+elif x == "86":
+    print("The ASCII Code 86 stands for the Alphabet V")
+elif x == "87":
+    print("The ASCII Code 87 stands for the Alphabet W")
+elif x == "88":
+    print("The ASCII Code 88 stands for the Alphabet X")
+elif x == "89":
+    print("The ASCII Code 89 stands for the Alphabet Y")
+elif x == "90":
+    print("The ASCII Code 90 stands for the Alphabet Z")
+else:
+    print("Invalid input. Please enter an ASCII Code between 65 and 90.")
