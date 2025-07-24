a70ddc1794d0d8216340b5d12d9e37cd09fdc3ea
arokya2648 <arokyasengooka@gmail.com>
2025-07-24 11:22:41 +0700

    reverse string

---
+++ reverser a string.py
@@ -0,0 +1,6 @@
+string=input('Please enter your own string:')
+string2=('')
+for i in string:
+    string2=i+string
+print('\nThe original string is:',string)
+print('\nThe reversed string is:',string2)
