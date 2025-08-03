56bd58b93692480687990791c08c73344cf5d02b
arokya2648 <arokyasengooka@gmail.com>
2025-08-03 20:16:44 +0700

    spiral

---
+++ spiral.py
@@ -0,0 +1,13 @@
+import turtle
+mywn=turtle.Screen()
+mywn.bgcolor("light blue")
+mywn.title("Turtle")
+pen=turtle.Turtle()
+size=0
+while True:
+    for i in range(4):
+        pen.fd(size+1)
+        pen.left(90)
+        size=size-5
+    size=size+1
+turtle.done()
