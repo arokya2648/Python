4f264d4eeaa71ab849d68d82cfcd705c7d98da90
arokya2648 <arokyasengooka@gmail.com>
2025-09-09 17:39:00 +0700

    parrot class

--- __pycache__/vehicle class.py
+++
@@ -1,7 +0,0 @@
-class vehicle:
-    def __init__(self, max_speed, mileage):
-        self.max_speed=max_speed
-        self.mileage=mileage
-modelx=vehicle(240, 18)
-print('mode1 max speed;',modelx.max_speed)
-print('model mileage:', modelx.mileage)w
