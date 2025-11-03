8fa6f3cb11e30600183e84066e0ad828aede7c83
arokya2648 <arokyasengooka@gmail.com>
2025-11-03 17:28:30 +0700

    number pad

---
+++ product widget.py
@@ -0,0 +1,6 @@
+from tkinter import *
+from datetime import date
+root=Tk()
+root.title('Getting Started With Widgets')
+root.geometry('400x300')
+lbl = Label(text='Lets Multiply Two Numbers', fg='white', bg='#072F5F', height=1, width=300)
