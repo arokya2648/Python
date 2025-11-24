f4005e36227b27887196acbb86219105cb405d87
arokya2648 <arokyasengooka@gmail.com>
2025-11-24 17:32:00 +0700

    ontime

---
+++ Ontime.py
@@ -0,0 +1,9 @@
+def ontime(n):
+    iterations=0
+    for i in range(1, n+1):
+        iterations+=1
+    print('when n  is', n ,'iteratons=', iterations)
+ontime(10)
+ontime(20)
+ontime(42)
+print('With every n the  time taken and iterrations will increase')
