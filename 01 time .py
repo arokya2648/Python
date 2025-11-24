670c67cc52e99eb9c7309e625689a64cfffd52a0
arokya2648 <arokyasengooka@gmail.com>
2025-11-24 17:22:21 +0700

    01 time

---
+++ 01 time .py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iteration =0
+    print('The number entered by the user is:', n)
+    iteration+=1
+    print('The total number of iterations done by the code:', iteration)
+printnumber(10)
+printnumber(20)
+print("with any 'n' the time taken by the code wont change")
