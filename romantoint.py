40e9886dc350bce87a61ae6706e09cdef471b0d9
arokya2648 <arokyasengooka@gmail.com>
2025-12-02 17:40:26 +0700

    romantoint

---
+++ romantoint.py
@@ -0,0 +1,11 @@
+def romantoint(romaninput):
+    roman={'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
+    resultinteger=0
+    for i in range(0, len(romaninput)-1):
+        if roman[romaninput[i]]  < roman[romaninput[i+1]]:
+            resultinteger-=roman[romaninput[i]]
+        else:
+            resultinteger+=roman[romaninput[i]]
+    return resultinteger+roman[romaninput[-1]]
+roman=input('Enter roman numeral')
+print('integer equivilent:', romantoint(roman))
