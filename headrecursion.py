3eb6ceddb3fec601d2a8829cea3b30f7e3a66028
arokya2648 <arokyasengooka@gmail.com>
2026-01-06 17:18:28 +0700

    headrec

--- headrecursion.py
+++ headrecursion.py
@@ -1,12 +1,12 @@
 def headrec(n, num):
-    #Base case, if n exceeds num terminate the rrecurrsion
+    #Base case, if n exceeds num terminate the recursion
     if n>num:
         return
-    #Recursive call with tthe next value
+    #Recursive call with the next value
     headrec(n+1, num)
     #Print the current value of n after returning the recursive call
     print(n)
-#promt the user to enter tthe value of n
+#promt the user to enter the value of n
 n=int(input('Enter the  value of n to print 1 to n:'))
 #initialize call to the headrec function starting from 1
 headrec(1, n)
