765f9f51547aaf90280d04946b31a4e776c20709
arokya2648 <arokyasengooka@gmail.com>
2026-01-12 20:43:30 +0700

    listlengtth

---
+++ listlength.py
@@ -0,0 +1,9 @@
+def list_length(my_list):
+    if not my_list:
+        return 0
+    return 1 + list_length(my_list[1::2]) + list_length(my_list[2::2])
+my_list = [1,2,3,11,34,52,72]
+print('The list is:')
+print(my_list)
+print('The length of the string is')
+print(list_length(my_list))
