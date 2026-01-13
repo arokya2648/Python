c43d95731ab3e5e324e706a7ab9949707723c416
arokya2648 <arokyasengooka@gmail.com>
2026-01-13 17:38:06 +0700

    powerof4rrecurrsion

---
+++ powerof4recurrsion.py
@@ -0,0 +1,17 @@
+#Program to check if the number is power of 4 or not
+#Take input
+n=int(input('Enter your number'))
+def checkifpower(n):
+    #if n  is less than equal to 0 just say no
+    if(n<=0):
+        return False
+    #if we reach lowest power of n just return true
+    if(n==1):
+        return True
+    if(n%4==0):
+        return checkifpower(n/4)
+    return False
+if(checkifpower(n)):
+    print('Power of 4')
+else:
+    print('Not a power of 4')
